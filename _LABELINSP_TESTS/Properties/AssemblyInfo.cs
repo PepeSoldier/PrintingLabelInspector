cd628c1e4308b3cd12c58a60ed9b7e6f187cd277
kamil <kam.krzyzanowski@gmail.com>
2020-09-29 12:45:53 +0200

    Rename to _LABELINSP

---
+++ _LABELINSP_TESTS/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("_LABELINSP_TESTS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("_LABELINSP_TESTS")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0d731d65-1d59-4fd4-9a0a-5ca425ceeb47")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
